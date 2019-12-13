d80d781eeb7fc15260428401dda723feab1d6ccc
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-13 05:19:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,10 +12,7 @@ def countdown_with_sleep(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number-=1
-    delay = 1
-    while delay > 0
-      delay -= 1
-    end
+    sleep(1)
   end
   return "HAPPY NEW YEAR!"
 end
